629bfa0af214455edcd9fabeac182266cad662ab
Akuto Sai <36693722+AkutoSai@users.noreply.github.com>
2019-12-28 05:25:46 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,5 @@ whoispassw = ''
 # https://useragentapi.com
 useragentapikey = ''
 
-# https://www.bincodes.com/api-creditcard-checker/
-cc = ''
-api_key = ''
-
 # https://whatcms.org
 whatcmskey = ''
